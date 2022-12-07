2ea303f3857e4047c7f555fa9a84dcdb932c7e4b
Darshan Jain <c0872398@mylambton.ca>
2022-12-07 18:23:44 -0500

    setup base activity

--- app/src/main/java/com/darshan09200/employeemanagement/MainActivity.java
+++ app/src/main/java/com/darshan09200/employeemanagement/MainActivity.java
@@ -2,13 +2,25 @@ package com.darshan09200.employeemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.darshan09200.employeemanagement.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.activity2.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+        });
     }
 }
