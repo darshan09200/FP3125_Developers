1ea576979ffad4bc07463a7d6491aedb3d365966
jayesh-khistria <c0880752@mylambton.ca>
2022-12-09 17:51:41 -0500

    added vehicle and vehicle make emun

---
+++ app/src/main/java/com/darshan09200/employeemanagement/RegistrationModel.java
@@ -0,0 +1,79 @@
+package com.darshan09200.employeemanagement;
+
+
+enum Vehicle {
+    NONE("None"),
+    CAR("Car"),
+    MOTORCYCLE("Motorcycle");
+
+    private String label;
+
+    Vehicle(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    static Vehicle getVehicle(String vehicle) {
+        switch (vehicle) {
+            case "Car":
+                return CAR;
+            case "Motorcycle":
+                return MOTORCYCLE;
+        }
+        return NONE;
+    }
+}
+
+enum VehicleMake {
+    CHOOSE_MAKE("Please choose a make", Vehicle.NONE),
+    KAWASAKI("Kawasaki", Vehicle.MOTORCYCLE),
+    HONDA("Honda", Vehicle.MOTORCYCLE),
+    LAMBORGHINI("Lamborghini", Vehicle.CAR),
+    BMW("BMW", Vehicle.CAR),
+    RENAULT("Renault", Vehicle.CAR),
+    MAZDA("Mazda", Vehicle.CAR);
+
+    private final String label;
+    private final Vehicle vehicle;
+
+    VehicleMake(String label, Vehicle vehicle) {
+        this.label = label;
+        this.vehicle = vehicle;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    static VehicleMake getVehicleMake(String vehicleMake) {
+        switch (vehicleMake) {
+            case "Please choose a make":
+                return CHOOSE_MAKE;
+            case "Kawasaki":
+                return KAWASAKI;
+            case "Honda":
+                return HONDA;
+            case "Lamborghini":
+                return LAMBORGHINI;
+            case "BMW":
+                return BMW;
+            case "Renault":
+                return RENAULT;
+            case "Mazda":
+                return MAZDA;
+        }
+        return null;
+    }
+}
+
+
+
+public class RegistrationModel {
+}
