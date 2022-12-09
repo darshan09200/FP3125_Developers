4192669e913c5fa8975ccdb1ede376d3955d9c40
Darshan Jain <c0872398@mylambton.ca>
2022-12-09 16:53:58 -0500

    introduced registration controller

--- app/src/main/java/com/darshan09200/employeemanagement/RegistrationActivity.java
+++ app/src/main/java/com/darshan09200/employeemanagement/RegistrationActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.darshan09200.employeemanagement.databinding.ActivityRegistrationBinding;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    ActivityRegistrationBinding binding;
+
+    RegistrationController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        controller = new RegistrationController(binding);
+        setContentView(binding.getRoot());
     }
 }
---
+++ app/src/main/java/com/darshan09200/employeemanagement/RegistrationController.java
@@ -0,0 +1,12 @@
+package com.darshan09200.employeemanagement;
+
+import com.darshan09200.employeemanagement.databinding.ActivityRegistrationBinding;
+
+public class RegistrationController {
+
+    ActivityRegistrationBinding binding;
+
+    public RegistrationController(ActivityRegistrationBinding binding) {
+        this.binding = binding;
+    }
+}
