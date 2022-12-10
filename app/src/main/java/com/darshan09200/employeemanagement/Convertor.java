0805e0e8a4b7d4cd318069c39b554cdf99ce47b7
Darshan Jain <c0872398@mylambton.ca>
2022-12-09 20:21:11 -0500

    added spinner data to class

---
+++ app/src/main/java/com/darshan09200/employeemanagement/Convertor.java
@@ -0,0 +1,43 @@
+package com.darshan09200.employeemanagement;
+
+public class Convertor {
+    public static Vehicle convertVehicle(String text) {
+        for (Vehicle v :
+                Vehicle.values()) {
+            if (v.getLabel().equalsIgnoreCase(text)) return v;
+        }
+        return null;
+    }
+
+    public static VehicleMake convertVehicleMake(String text) {
+        for (VehicleMake v :
+                VehicleMake.values()) {
+            if (v.getLabel().equalsIgnoreCase(text)) return v;
+        }
+        return null;
+    }
+
+    public static VehicleCategory convertVehicleCategory(String text) {
+        for (VehicleCategory v :
+                VehicleCategory.values()) {
+            if (v.getLabel().equalsIgnoreCase(text)) return v;
+        }
+        return null;
+    }
+
+    public static VehicleType convertVehicleType(String text) {
+        for (VehicleType v :
+                VehicleType.values()) {
+            if (v.getLabel().equalsIgnoreCase(text)) return v;
+        }
+        return null;
+    }
+
+    public static VehicleColor convertVehicleColor(String text) {
+        for (VehicleColor v :
+                VehicleColor.values()) {
+            if (v.getLabel().equalsIgnoreCase(text)) return v;
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/darshan09200/employeemanagement/Registration.java
@@ -0,0 +1,175 @@
+package com.darshan09200.employeemanagement;
+
+
+import java.util.ArrayList;
+
+enum Vehicle {
+    BOTH("Both"),
+    CAR("Car"),
+    MOTORCYCLE("Motorcycle");
+
+    private final String label;
+
+    Vehicle(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
+
+enum VehicleMake {
+    CHOOSE_MAKE("Please choose a make", Vehicle.BOTH),
+    KAWASAKI("Kawasaki", Vehicle.MOTORCYCLE),
+    HONDA("Honda", Vehicle.BOTH),
+    LAMBORGHINI("Lamborghini", Vehicle.CAR),
+    BMW("BMW", Vehicle.CAR),
+    RENAULT("Renault", Vehicle.CAR),
+    MAZDA("Mazda", Vehicle.CAR);
+
+    private final String label;
+    private final Vehicle vehicle;
+
+    VehicleMake(String label, Vehicle vehicle) {
+        this.label = label;
+        this.vehicle = vehicle;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+}
+
+enum VehicleCategory {
+    CHOSE_CATEGORY("Please choose a category", Vehicle.BOTH),
+    RACE_MOTORCYCLE("Race Motorcycle", Vehicle.MOTORCYCLE),
+    NOT_FOR_RACE("Not for Race", Vehicle.BOTH),
+    FAMILY("Family", Vehicle.CAR);
+
+    private final String label;
+    private final Vehicle vehicle;
+
+    VehicleCategory(String label, Vehicle vehicle) {
+        this.label = label;
+        this.vehicle = vehicle;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+}
+
+enum VehicleType {
+    CHOOSE_TYPE("Please choose a type"),
+    SEDAN("Sedan"),
+    SPORT("Sport"),
+    HATCHBACK("Hatchback"),
+    SUV("SUV");
+
+    private final String label;
+
+    VehicleType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
+
+enum VehicleColor {
+    CHOOSE_COLOR("Please choose a color"),
+    YELLOW("Yellow"),
+    BLACK("Black"),
+    WHITE("White"),
+    RED("Red");
+
+    private final String label;
+
+    VehicleColor(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
+
+
+public class Registration {
+    private static Registration instance;
+
+    private Vehicle vehicle;
+
+    private Registration() {
+    }
+
+    public static Registration getInstance() {
+        if (instance == null) instance = new Registration();
+        return instance;
+    }
+
+    public ArrayList<String> getVehicleData() {
+        ArrayList<String> vehicles = new ArrayList<>();
+
+        for (Vehicle vehicle :
+                Vehicle.values()) {
+            if (vehicle != Vehicle.BOTH) vehicles.add(vehicle.getLabel());
+        }
+        return vehicles;
+    }
+
+    public ArrayList<String> getVehicleMakeData() {
+        ArrayList<String> vehicleMakes = new ArrayList<>();
+
+        for (VehicleMake vehicleMake :
+                VehicleMake.values()) {
+            if (vehicleMake.getVehicle() == vehicle || vehicleMake.getVehicle() == Vehicle.BOTH)
+                vehicleMakes.add(vehicleMake.getLabel());
+        }
+        return vehicleMakes;
+    }
+
+    public ArrayList<String> getVehicleCategoryData() {
+        ArrayList<String> vehicleCategories = new ArrayList<>();
+
+        for (VehicleMake vehicleCategory :
+                VehicleMake.values()) {
+            if (vehicleCategory.getVehicle() == vehicle || vehicleCategory.getVehicle() == Vehicle.BOTH)
+                vehicleCategories.add(vehicleCategory.getLabel());
+        }
+        return vehicleCategories;
+    }
+
+    public ArrayList<String> getVehicleTypeData() {
+        ArrayList<String> vehicleTypes = new ArrayList<>();
+
+        for (VehicleMake vehicleType :
+                VehicleMake.values()) {
+            if (vehicleType.getVehicle() == vehicle || vehicleType.getVehicle() == Vehicle.BOTH)
+                vehicleTypes.add(vehicleType.getLabel());
+        }
+        return vehicleTypes;
+    }
+
+    public ArrayList<String> getVehicleColorData() {
+        ArrayList<String> vehicleColors = new ArrayList<>();
+
+        for (VehicleMake vehicleColor :
+                VehicleMake.values()) {
+            if (vehicleColor.getVehicle() == vehicle || vehicleColor.getVehicle() == Vehicle.BOTH)
+                vehicleColors.add(vehicleColor.getLabel());
+        }
+        return vehicleColors;
+    }
+
+}
