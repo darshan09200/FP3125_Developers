e45a40cd7e995f6d65d710f0b75a11367f75e4b4
Darshan Jain <c0872398@mylambton.ca>
2022-12-11 19:03:04 -0500

    added listview on home screen and search in toolbar

--- app/src/main/java/com/darshan09200/employeemanagement/MainActivity.java
+++ app/src/main/java/com/darshan09200/employeemanagement/MainActivity.java
@@ -1,16 +1,30 @@
 package com.darshan09200.employeemanagement;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.darshan09200.employeemanagement.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    ArrayList<Employee> employees;
+    ArrayAdapter<Employee> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +33,72 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
-        binding.activity2.setOnClickListener(v -> {
+        employees = new ArrayList<>();
+        employees.addAll(Database.getInstance().getEmployees());
+        System.out.println(employees);
+        adapter = new ArrayAdapter<Employee>(this, android.R.layout.simple_list_item_2, android.R.id.text1, employees) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = view.findViewById(android.R.id.text1);
+                TextView text2 = view.findViewById(android.R.id.text2);
+                Employee employee = getItem(position);
+                text1.setText(employee.getName());
+                text2.setText(employee.getRole().getLabel());
+                return view;
+            }
+        };
+        adapter.setNotifyOnChange(true);
+        binding.listView.setAdapter(adapter);
+        binding.listView.setOnItemClickListener((parent, view, position, id) -> {
+            System.out.println(parent.getAdapter() + " " + position);
+        });
+
+        binding.addEmployee.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        MenuItem searchViewItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // If the list contains the search query than filter the adapter
+                // using the filter method with the query as its argument
+//                if (employees.contains(query)) {
+//                    adapter.getFilter().filter(query);
+//                } else {
+//                    // Search query not found in List View
+//                    Toast.makeText(MainActivity.this, "Not found", Toast.LENGTH_LONG).show();
+//                }
+                return false;
+            }
+
+            // This method is overridden to filter the adapter according
+            // to a search query when the user is typing search
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        employees.clear();
+        employees.addAll(Database.getInstance().getEmployees());
+        adapter.notifyDataSetChanged();
     }
 }
